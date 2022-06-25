ad989c3f6b21f8cc9887380eacb8bf36632ec01f
goku <goku@goku.com>
2022-06-25 16:16:35 +0800

    2-7 增加hello接口测试test.http

--- src/main/java/com/goku/wiki/controller/TestController.java
+++ src/main/java/com/goku/wiki/controller/TestController.java
@@ -1,14 +1,21 @@
 package com.goku.wiki.controller;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class TestController {
-    @RequestMapping("/hello")
-    public String Hello() {
+    @GetMapping("/hello")
+    public String hello() {
         return "Hello World!";
+    }
+
+    @PostMapping("hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post "+name;
     }
 }
