79717f584c2bca773bf8f640d63d0bd3e16dc7c0
goku <fasds@163.com>
2022-07-03 17:38:11 +0800

    5-4 后端数据绑定到前端

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -48,24 +48,30 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      Content
+      <pre>{{ ebooks }}</pre>
     </a-layout-content>
   </a-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref,onMounted } from 'vue';
 import axios from "axios";
 
 export default defineComponent({
   name: 'HomeView',
-  setup(){
+  setup() {
     console.log("setup")
-    axios.get("http://localhost:8880/ebook/list?name=spring").then(
-        (response)=>{
-          console.log(response)
-        }
-    )
+    const ebooks = ref()
+    onMounted(() => {
+      console.log('onMounted')
+      axios.get("http://localhost:8880/ebook/list?name=spring").then(
+          (response) => {
+            const data=response.data;
+            ebooks.value=data.content;
+            console.log(response)
+          });
+    })
+    return {ebooks}
   }
-});
+  });
 </script>
