243011539a1124b7b8534e6b88bd7c8af9ce2c2c
goku <fasds@163.com>
2022-07-24 14:59:27 +0800

    7-4 分类管理不显示分页

--- src/main/java/com/goku/wiki/controller/CategoryController.java
+++ src/main/java/com/goku/wiki/controller/CategoryController.java
@@ -8,9 +8,11 @@ import com.goku.wiki.resp.PageResp;
 import com.goku.wiki.service.CategoryService;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.web.bind.annotation.*;
+import org.w3c.dom.stylesheets.LinkStyle;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.List;
 
 
 @RestController
@@ -22,6 +24,13 @@ public class CategoryController {
     @Resource
     private CategoryService categoryService;
 
+    @GetMapping("/all")
+    public CommonResp all() {
+        CommonResp<List<CategoryQueryResp>> resp = new CommonResp<>();
+        List<CategoryQueryResp> list =categoryService.all();
+        resp.setContent(list);
+        return resp;
+    }
 
     @GetMapping("/list")
     public CommonResp list(@Valid CategoryQueryReq req) {
