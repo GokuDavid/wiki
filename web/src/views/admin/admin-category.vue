a69e68c7b2642b05c3579874a57847ddc0ee2c70
goku <fasds@163.com>
2022-07-24 15:08:14 +0800

    7-4 分类管理树形展示，增加树形结构方法

--- web/src/views/admin/admin-category.vue
+++ web/src/views/admin/admin-category.vue
@@ -20,7 +20,7 @@
       <a-table
           :columns="columns"
           :row-key="record => record.id"
-          :data-source="categorys"
+          :data-source="level1"
           :loading="loading"
           :pagination="false"
       >
@@ -99,6 +99,19 @@ export default defineComponent({
     ];
 
     /**
+     * 一级分类树，children属性就是二级分类
+     * [{
+     *   id: "",
+     *   name: "",
+     *   children: [{
+     *     id: "",
+     *     name: "",
+     *   }]
+     * }]
+     */
+    const level1=ref();
+
+    /**
      * 数据查询
      **/
     const handleQuery = () => {
@@ -108,6 +121,10 @@ export default defineComponent({
         const data = response.data;
         if (data.success){
           categorys.value = data.content;
+          console.log("原始数组：", categorys.value);
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys.value, 0);
+          console.log("树形结构：", level1);
 
         }else {
           message.error(data.message);
@@ -169,7 +186,8 @@ export default defineComponent({
     });
 
     return {
-      categorys,
+      level1,
+      // categorys,
       columns,
       loading,
       category,
