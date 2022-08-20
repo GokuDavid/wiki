f37c1cb422da04bc87405d34e70c4f33538037b5
goku <fasds@163.com>
2022-08-20 15:37:55 +0800

    8-5 增加ebookid参数传递

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -64,15 +64,6 @@
         >
         </a-tree-select>
       </a-form-item>
-      <a-form-item label="父文档">
-        <a-select
-            ref="select"
-            v-model:value="doc.parent"
-        >
-          <a-select-option value="0">无</a-select-option>
-          <a-select-option v-for="c in level1" :key="c.id" :value="c.id" :disabled="doc.id===c.id">{{ c.name}}</a-select-option>
-        </a-select>
-    </a-form-item>
       <a-form-item label="排序">
         <a-input v-model:value="doc.sort" />
       </a-form-item>
@@ -85,11 +76,20 @@ import { defineComponent, onMounted, ref } from 'vue';
 import axios from 'axios';
 import {message} from "ant-design-vue";
 import {Tool} from "@/util/tool";
+import {useRoute} from "vue-router";
 
 
 export default defineComponent({
   name: 'AdminDoc',
   setup() {
+    const route=useRoute();
+    console.log("路由：", route);
+    console.log("route.path：", route.path);
+    console.log("route.query：", route.query);
+    console.log("route.param：", route.params);
+    console.log("route.fullPath：", route.fullPath);
+    console.log("route.name：", route.name);
+    console.log("route.meta：", route.meta);
     const param=ref();
     param.value={};
     const docs = ref();
@@ -223,7 +223,9 @@ export default defineComponent({
      */
     const add = () => {
       modalVisible.value = true;
-      doc.value={};
+      doc.value={
+        ebookId:route.query.ebookId
+      };
       treeSelectData.value = Tool.copy(level1.value);
 
       // 为选择树添加一个"无"
