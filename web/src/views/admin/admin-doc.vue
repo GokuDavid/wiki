394da735ef5ed9264ef16dcf663224bd2dc9563f
goku <fasds@163.com>
2022-09-04 17:10:23 +0800

    8-9 优化文档编辑页面，初始让树展开

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -3,7 +3,7 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <a-row>
+      <a-row :gutter="24">
         <a-col :span="8">
           <p>
           <a-form layout="inline" :model="param">
@@ -20,18 +20,21 @@
           </a-form>
         </p>
           <a-table
+              v-if="level1.length>0"
               :columns="columns"
               :row-key="record => record.id"
               :data-source="level1"
               :loading="loading"
               :pagination="false"
+              size="small"
+              :defaultExpandAllRows="true"
           >
-            <template #cover="{ text: cover }">
-              <img v-if="cover" :src="cover" alt="avatar" />
+            <template #name="{ text, record  }">
+              {{record.sort}} {{text}}
             </template>
             <template v-slot:action="{ text, record }">
               <a-space size="small">
-                <a-button type="primary" @click="edit(record)">
+                <a-button type="primary" @click="edit(record)" size="small">
                   编辑
                 </a-button>
                 <a-popconfirm
@@ -40,7 +43,7 @@
                     cancel-text="否"
                     @confirm="handleDelete(record.id)"
                 >
-                  <a-button type="danger">
+                  <a-button type="danger" size="small">
                     删除
                   </a-button>
                 </a-popconfirm>
@@ -48,11 +51,20 @@
             </template>
           </a-table></a-col>
         <a-col :span="16">
-          <a-form :model="doc" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
-            <a-form-item label="名称">
-              <a-input v-model:value="doc.name" />
+          <p>
+            <a-form layout="inline" :model="param">
+              <a-form-item>
+                <a-button type="primary" @click="handleSave()">
+                  保存
+                </a-button>
+              </a-form-item>
+            </a-form>
+          </p>
+          <a-form :model="doc" layout="vertical">
+            <a-form-item>
+              <a-input v-model:value="doc.name" placeholder="名称"/>
             </a-form-item>
-            <a-form-item label="父文档">
+            <a-form-item>
               <a-tree-select
                   v-model:value="doc.parent"
                   style="width: 100%"
@@ -64,10 +76,10 @@
               >
               </a-tree-select>
             </a-form-item>
-            <a-form-item label="排序">
-              <a-input v-model:value="doc.sort" />
+            <a-form-item>
+              <a-input v-model:value="doc.sort" placeholder="顺序"/>
             </a-form-item>
-            <a-form-item label="内容">
+            <a-form-item>
               <div id="content"></div>
             </a-form-item>
           </a-form>
@@ -109,16 +121,8 @@ export default defineComponent({
     const columns = [
       {
         title: '名称',
-        dataIndex: 'name'
-      },
-      {
-        title: '父文档',
-        key:'parent',
-        dataIndex: 'parent',
-      },
-      {
-        title: '顺序',
-        dataIndex: 'sort',
+        dataIndex: 'name',
+        slots:{customRender: 'name'}
       },
       {
         title: 'Action',
@@ -139,6 +143,7 @@ export default defineComponent({
      * }]
      */
     const level1=ref();
+    level1.value=[];
 
     /**
      * 数据查询
@@ -172,8 +177,9 @@ export default defineComponent({
     const modalVisible = ref(false);
     const modalLoading = ref(false);
     const editor = new E("#content");
+    editor.config.zIndex=0;
 
-    const handleModalOk = () => {
+    const handleSave = () => {
       modalLoading.value = true;
       axios.post("/doc/save",
           doc.value).then((response) => {
@@ -265,7 +271,7 @@ export default defineComponent({
       // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
       setTimeout(function ()
-          {editor.create();},1000
+          {editor.create();},100
       );
     };
 
@@ -282,7 +288,7 @@ export default defineComponent({
       // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
       setTimeout(function (){
-          editor.create();},1000);
+          editor.create();},100);
     };
 
     /**
@@ -314,7 +320,7 @@ export default defineComponent({
       handleDelete,
       modalLoading,
       modalVisible,
-      handleModalOk,
+      handleSave,
       handleQuery,
       param,
       treeSelectData
